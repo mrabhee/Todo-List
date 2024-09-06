ed49404f24ec5efc83791ad848cd747e0a418d13
Abhishek Patidar <107181809+mrabhee@users.noreply.github.com>
2024-09-06 14:03:51 +0530

    done

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { database } from '../Appwrite/config'
 import { collectionId, databaseId } from '../Appwrite/appwriteid'
 import { Query } from 'appwrite'
@@ -32,14 +32,14 @@ const TodoList = ({ emailid }) => {
         }
     }
 
-    const handleView = useCallback(async () => {
+    const handleView = async () => {
         try {
             const dbtodo = await database.listDocuments(databaseId, collectionId, [Query.equal('email', emailid)])
             setViewtodo(dbtodo.documents)
         } catch (error) {
             console.log(error)
         }
-    },[emailid])
+    }
 
     const handleDelete = async (id) => {
         try {
@@ -48,11 +48,12 @@ const TodoList = ({ emailid }) => {
         } catch (error) {
             console.log(error)
         }
-    }
+    } 
+
 
     useEffect(() => {
-        console.log("useeffect todo")
-    }, [handleView])
+        handleView()
+    }, [emailid])
 
     return (
         <>
