dadee9762b4afcc915a10c85d5cde1be5eabba8c
Abhishek Patidar <107181809+mrabhee@users.noreply.github.com>
2024-09-06 13:52:58 +0530

    kya h re

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -39,7 +39,7 @@ const TodoList = ({ emailid }) => {
         } catch (error) {
             console.log(error)
         }
-    },[handleView])
+    },[emailid])
 
     const handleDelete = async (id) => {
         try {
